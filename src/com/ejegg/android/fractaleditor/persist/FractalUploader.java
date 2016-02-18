7bdb1ec8337b20baabf810784ec265d5e6beb614
Elliott Eggleston <ejegg@ejegg.com>
2016-02-18 01:23:19 -0500

    Upload rendered image to server
    Not so much a thumbnail as a big ol picture

--- src/com/ejegg/android/fractaleditor/persist/FractalUploader.java
+++ src/com/ejegg/android/fractaleditor/persist/FractalUploader.java
@@ -1,5 +1,7 @@
 package com.ejegg.android.fractaleditor.persist;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -25,22 +27,48 @@ public class FractalUploader extends AsyncTask<FractalState, String, Boolean>{
 	protected Boolean doInBackground(FractalState... params) {
 		HttpURLConnection connection = null;
 		Boolean success = true;
+		FractalState state = params[0];
+		String thumbnailPath = state.getThumbnailPath();
+
 		try {
-			FractalState state = params[0];
+			File thumbnail = new File(thumbnailPath);
 			connection = (HttpURLConnection)url.openConnection();
-			byte[] postBytes = String.format(
-					"name=%s&serializedTransforms=%s",
-					URLEncoder.encode( state.getName(), "UTF-8" ),
-					URLEncoder.encode( state.getSerializedTransforms(), "ASCII" )
-			).getBytes("UTF-8");
 			connection.setDoOutput(true);
 			connection.setRequestProperty("Accept-Charset", "UTF-8");
-			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			connection.setRequestProperty("Content-Length", Integer.toString(postBytes.length));
+			connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=X*X*X*X*X*X*X*X");
 
 			OutputStream out = connection.getOutputStream();
-			out.write(postBytes);
+			String boundary = "X*X*X*X*X*X*X*X";
+			String fieldFormat = "--%s\r\n" +
+					"Content-Disposition: form-data; name=\"%s\"\r\n" +
+					"Content-Type: text/plain; charset=\"UTF-8\"\r\n\r\n" +
+					"%s\r\n";
+			String fileFormat = "--%s\r\n" +
+					"Content-Disposition: form-data; name=\"%s\"; filename=\"%s\"\r\n" +
+					"Content-Type: image/png\r\n" +
+					"Content-Transfer-Encoding: binary\r\n\r\n";
+			String formFields = String.format(fieldFormat + fieldFormat + fileFormat,
+					boundary,
+					"name", state.getName(),
+					boundary,
+					"serializedTransforms", state.getSerializedTransforms(),
+					boundary,
+					"thumbnail", thumbnail.getName());
+			out.write(formFields.getBytes("UTF-8"));
+			out.flush();
+			FileInputStream thumbStream = new FileInputStream(thumbnail);
+			byte[] buffer = new byte[4096];
+			int bytesRead;
+			while ((bytesRead = thumbStream.read(buffer)) != -1) {
+				out.write(buffer, 0, bytesRead);
+				// TODO: compare total read with file size, post progress
+			}
+			out.write(String.format("\r\n--%s--\r\n", boundary).getBytes("UTF-8"));
+			out.flush();
+			out.close();
+			thumbStream.close();
 
+			// TODO: something with connection.getResponseCode();
 			int responseLength = Math.min(1000, Integer.parseInt(connection.getHeaderField("Content-Length")));
 			byte[] responseBuffer = new byte[responseLength];
 			connection.getInputStream().read(responseBuffer);
